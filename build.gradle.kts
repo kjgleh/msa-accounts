fb59db483a1a616fefb617d1f6bc5830b8112e68
kjg.leh <kjg.leh@gmail.com>
2021-12-25 17:44:51 +0900

    set mysql

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    // spring data jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // mysql
+    runtimeOnly("mysql:mysql-connector-java")
+
     // spring cloud config
     implementation("org.springframework.cloud:spring-cloud-starter-config")
 
